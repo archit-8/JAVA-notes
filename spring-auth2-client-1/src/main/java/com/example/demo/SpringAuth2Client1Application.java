965785a8ff934bddde77ea381b2faadb059b7dec
Archit Singh Chauhan <88979004+archit-8@users.noreply.github.com>
2025-12-15 04:33:53 +0000

    Add files via upload

---
+++ spring-auth2-client-1/src/main/java/com/example/demo/SpringAuth2Client1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuth2Client1Application {
+
+	public static void main(String[] args) {
+		System.out.println("deds");
+		SpringApplication.run(SpringAuth2Client1Application.class, args);
+	}
+
+}
